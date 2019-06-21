52d91c8f63e33771f541f61d6a3ee8ccb2d71996
Sergio Hernandez <sergio.hernandez@cnn.com>
2019-06-21 13:10:36 -0400

    use 2.0 endpoint

--- lib/omniauth/strategies/azure_activedirectory.rb
+++ lib/omniauth/strategies/azure_activedirectory.rb
@@ -195,7 +195,7 @@ module OmniAuth
       #
       # @return String
       def openid_config_url
-        "https://login.windows.net/#{tenant}/.well-known/openid-configuration"
+        "https://login.windows.net/#{tenant}/v2.0/.well-known/openid-configuration"
       end
 
       ##
